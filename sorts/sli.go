ce36cf200a015260f74f760227c8790414f6cbad
pyihe <785131182@qq.com>
2021-07-22 16:17:27 +0800

    fix: 增加uint和float64

--- sorts/sli.go
+++ sorts/sli.go
@@ -6,34 +6,45 @@ type Uint8Slice []uint8
 type Int8Slice []int8
 type Uint16Slice []uint16
 type Int16Slice []int16
+type IntSlice []int
+type UintSlice []uint
 type Int32Slice []int32
 type Uint32Slice []uint32
 type Int64Slice []int64
 type Uint64Slice []uint64
 type Float32Slice []float32
+type Float64Slice []float64
 type StringSlice []string
 
-func SortUint8(s []uint8) {
+func SortUint8s(s []uint8) {
 	sort.Sort(Uint8Slice(s))
 }
 
-func SortInt8(s []int8) {
+func SortInt8s(s []int8) {
 	sort.Sort(Int8Slice(s))
 }
 
-func SortUint16(s []uint16) {
+func SortUint16s(s []uint16) {
 	sort.Sort(Uint16Slice(s))
 }
 
-func SortInt16(s []int16) {
+func SortInt16s(s []int16) {
 	sort.Sort(Int16Slice(s))
+}
+
+func SortInts(s []int) {
+	sort.Sort(IntSlice(s))
+}
+
+func SortUints(s []uint) {
+	sort.Sort(UintSlice(s))
 }
 
 func SortInt32s(s []int32) {
 	sort.Sort(Int32Slice(s))
 }
 
-func SortUint32(s []uint32) {
+func SortUint32s(s []uint32) {
 	sort.Sort(Uint32Slice(s))
 }
 
@@ -41,8 +52,12 @@ func SortInt64s(s []int64) {
 	sort.Sort(Int64Slice(s))
 }
 
-func SortUint64(s []uint64) {
+func SortUint64s(s []uint64) {
 	sort.Sort(Uint64Slice(s))
+}
+
+func SortFloat64s(s []float64) {
+	sort.Sort(Float64Slice(s))
 }
 
 func SortFloat32s(s []float32) {
@@ -98,6 +113,30 @@ func (i16s Int16Slice) Less(i, j int) bool {
 }
 
 func (i16s Int16Slice) Swap(i, j int) {
+	i16s[i], i16s[j] = i16s[j], i16s[i]
+}
+
+func (i16s IntSlice) Len() int {
+	return len(i16s)
+}
+
+func (i16s IntSlice) Less(i, j int) bool {
+	return i16s[i] < i16s[j]
+}
+
+func (i16s IntSlice) Swap(i, j int) {
+	i16s[i], i16s[j] = i16s[j], i16s[i]
+}
+
+func (i16s UintSlice) Len() int {
+	return len(i16s)
+}
+
+func (i16s UintSlice) Less(i, j int) bool {
+	return i16s[i] < i16s[j]
+}
+
+func (i16s UintSlice) Swap(i, j int) {
 	i16s[i], i16s[j] = i16s[j], i16s[i]
 }
 
@@ -159,6 +198,18 @@ func (f32s Float32Slice) Less(i, j int) bool {
 
 func (f32s Float32Slice) Swap(i, j int) {
 	f32s[i], f32s[j] = f32s[j], f32s[i]
+}
+
+func (f64s Float64Slice) Len() int {
+	return len(f64s)
+}
+
+func (f64s Float64Slice) Less(i, j int) bool {
+	return f64s[i] < f64s[j]
+}
+
+func (f64s Float64Slice) Swap(i, j int) {
+	f64s[i], f64s[j] = f64s[j], f64s[i]
 }
 
 func (s StringSlice) Len() int {
