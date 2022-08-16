b61575b24296a7f5c7c0ab05fdbee386e70b5590
pyihe <785131182@qq.com>
2022-08-16 14:15:32 +0800

    add time format

---
+++ times/consts.go
@@ -0,0 +1,8 @@
+package times
+
+const (
+	SlashFormat         = "2006/01/02 15:04:05"
+	SlashWithMillFormat = "2006/01/02 15:04:05.000000"
+	DashFormat          = "2006-01-02 15:04:05"
+	DashWithMillFormat  = "2006-01-02 15:04:05.000000"
+)
